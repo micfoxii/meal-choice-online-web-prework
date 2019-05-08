7e28a3b6d64d45093d76d0597df96db848d6d654
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-08 23:08:57 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,5 +1,5 @@
 def meal_choice(default=arg1="meat", arg2="vegan")
-  "#{arg}"
+  "#{arg1}"
 end
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
